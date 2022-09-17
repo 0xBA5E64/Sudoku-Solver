462aa5f654604b59bfafcbabca1ac0b71b899339
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-09-17 17:52:49 +0200

    Update indentation in main.py to match that of lib.py

--- src/main.py
+++ src/main.py
@@ -3,28 +3,28 @@ import logging
 # logging.basicConfig(format="[%(asctime)s] %(levelname)s: %(message)s", level=logging.DEBUG)
 
 def main():
-  my_sudoku = Sudoku([
-      0,0,0,2,3,0,1,8,0,
-      0,4,0,0,0,0,0,3,0,
-      1,0,0,6,0,9,7,0,0,
-      0,7,4,8,0,3,0,0,0,
-      0,0,0,0,9,0,6,0,0,
-      3,0,9,0,0,0,0,7,0,
-      2,0,3,5,8,6,0,1,7,
-      0,8,0,0,0,0,0,6,2,
-      4,6,1,3,7,0,0,5,9,
-  ])
+    my_sudoku = Sudoku([
+        0,0,0,2,3,0,1,8,0,
+        0,4,0,0,0,0,0,3,0,
+        1,0,0,6,0,9,7,0,0,
+        0,7,4,8,0,3,0,0,0,
+        0,0,0,0,9,0,6,0,0,
+        3,0,9,0,0,0,0,7,0,
+        2,0,3,5,8,6,0,1,7,
+        0,8,0,0,0,0,0,6,2,
+        4,6,1,3,7,0,0,5,9,
+    ])
 
-  print("00 - Untouched:")
-  my_sudoku.print()
+    print("00 - Untouched:")
+    my_sudoku.print()
 
-  solve_absolutes(my_sudoku)
-  print("01 - Absolutes solved:")
-  my_sudoku.print()
+    solve_absolutes(my_sudoku)
+    print("01 - Absolutes solved:")
+    my_sudoku.print()
 
-  reduced_brute(my_sudoku)
-  print("02 - Reduced brute-force attack")
-  my_sudoku.print()
+    reduced_brute(my_sudoku)
+    print("02 - Reduced brute-force attack")
+    my_sudoku.print()
 
 if __name__ == "__main__":
-  main()
+    main()
